e660e60a3d28937ea15b43cd23fb4d66e701848b
Mohd-kaif-ali <skaif5719@gmail.com>
2026-02-14 23:14:57 +0530

    Configure Cloud Redis with ioredis and dotenv

--- server/redis/client.js
+++ server/redis/client.js
@@ -1,10 +1,14 @@
-const Redis = require('ioredis');
 require('dotenv').config();
+const Redis = require('ioredis');
+
+if (!process.env.REDIS_URL) {
+    console.warn('REDIS_URL not found, using default localhost');
+}
 
 const redis = new Redis(process.env.REDIS_URL);
 
 redis.on('connect', () => {
-    console.log('Redis connected');
+    console.log('Redis connected successfully');
 });
 
 redis.on('error', (err) => {
